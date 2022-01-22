5d09796d7b250dccf5ff25ca9c291d97bc7f1513
Pengdwende Abdoul GUira <guimani@yahoo.fr>
2022-01-22 16:49:25 -0500

    added implementation on the Debit component that will allow us to view the actual account balance base on the debit amount and also view the current balance after every debit amount

--- src/components/Debit.js
+++ src/components/Debit.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 
 class Debit extends Component {
+
+    state = {
+        total:14568.27
+    };
     
     render() {
         const debitData = this.props.debitData
@@ -11,17 +15,28 @@ class Debit extends Component {
             <div>
                 
                 { debitData.map((debitItem) => (
+
+
                     <ul key={debitItem.id}>
                         <li>Description: {debitItem.description}</li> 
-                        <li>  Amount: {debitItem.amount}</li> 
+
+                        <li> 
+                            
+                            Amount: {debitItem.amount } <br />
+                            Available Balance Now Is: {this.state.total = parseFloat(this.state.total).toFixed(4)  - debitItem.amount }
+
+                        </li> 
+
                         <li>  Date: {debitItem.date}</li> 
                     </ul>
                     
                     
                 )
-                )}
+                )
                 
-                 <AccountBalance accountBalance={this.props.accountBalance}/>    
+                }
+                
+                 <AccountBalance accountBalance={this.state.total}/>    
             </div>
             
         );
